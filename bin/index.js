e283d6411697f89a92b2b81de896441c074196dd
LC <ahlc@sina.cn>
2021-06-20 22:00:55 +0800

    update index.js.

--- bin/index.js
+++ bin/index.js
@@ -3,9 +3,9 @@ const os = require('os')
 const fs = require('fs')
 const chalk = require('chalk')
 
-console.log('\n ============================')
-console.log(' |  ' + chalk.white('cmd-man') + '  |')
-console.log(' ============================\n')
+console.log('\n ======================================')
+console.log(' |  ' + chalk.white('cmd-man ') + chalk.green.bold.bgWhite('Show Your PC Env') + '  |')
+console.log(' ======================================\n')
 
 const { Command } = require('commander') // (normal include)
 const program = new Command()
@@ -13,7 +13,7 @@ const program = new Command()
 program
   .version('0.1.0')
   .description('Show Your PC Env')
-  .option('-l, --language <default zh>', 'select language')
+  .option('-l, --language <default zh>', 'select language => zh | en')
   .option('-a, --all', 'output all information')
   .option('-ip,--ip', 'output ip address')
   .option('-os,--os', 'output operator system')
@@ -26,6 +26,7 @@ program
 
 // console.log(program.usage())
 
+// process.argv 数组下标2之后 是实际的入参 所以 process.argv.slice(2) 截取一下
 if (!process.argv.slice(2).length) {
   program.outputHelp()
   // return
@@ -36,6 +37,7 @@ program.parse(process.argv)
 const options = program.opts()
 
 const lan = options.language || 'zh'
+
 const lanMap = {
   en: {
     ip: ' IP           : ',
@@ -56,53 +58,47 @@ const lanMap = {
     host: ' Host配置    : '
   }
 }
+
 // console.log(lanMap)
 // console.log(lanMap.en)
+
 const label = lanMap[lan]
+
 const map = {
   darwin: 'Mac OSX', linux: 'linux', win32: 'Windows x32', win64: 'Windows x64'
 }
+
 const cmdMap = {
   ip: function (label) {
     console.log(chalk.green(label.ip), os.networkInterfaces().en0[1].address)
   },
-
   os: function (label) {
     console.log(chalk.green(label.os), (map[process.platform] || 'Unknow'))
   },
-
   arch: function (label) {
     console.log(chalk.green(label.arch), os.arch())
   },
-
   tm: function (label) {
     console.log(chalk.green(label.tm), (os.totalmem() / Math.pow(2, 30) + 'GB'))
   },
-
   fm: function (label) {
     const pow = os.freemem() >= Math.pow(2, 30) ? 30 : 20
     const unit = pow === 30 ? 'GB' : 'MB'
-
     console.log(chalk.green(label.fm), (parseInt(os.freemem() / Math.pow(2, pow)) + unit))
   },
-
   cpu: function (label) {
     console.log(chalk.green(label.cpu), os.cpus()[0].model)
   },
-
   host: function (label) {
     if (process.platform !== 'darwin') {
       return console.log(chalk.green(label.host), 'Sorry, output host just support in Mac OSX.')
     }
-
     const hosts = fs.readFileSync('/etc/hosts').toString('utf-8').split(/\s+/g)
     const pureHosts = []
     let hostString = ''
-
     for (let i = 0; i < hosts.length; i++) {
       if (hosts[i]) pureHosts.push(hosts[i])
     }
-
     for (let j = 0; j < pureHosts.length; j++) {
       const prefix = (j === 0) ? '' : ' ||  '
 
@@ -116,7 +112,6 @@ const cmdMap = {
     }
     console.log(chalk.green(label.host), hostString)
   },
-
   me: function () {
     console.log('\n [ Github: https://github.com/ahviplc/cmd-man, by LC ]\n')
   }
